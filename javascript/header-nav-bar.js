86287e65389860986958e7afecbe6aef14059fa1
SouICry <souicry.email@gmail.com>
2014-08-24 19:11:07 -0700

    Menu fix; test android browser fix

--- javascript/header-nav-bar.js
+++ javascript/header-nav-bar.js
@@ -10,7 +10,7 @@ document.write("
 	</div>
 	<div id='header-content'>
 		<a href='index.html'>
-		<imgsrc='avatar-logo.png'/>
+		<img src='avatar-logo.png'/>
 		</a>
 	</div>
 </div>
@@ -20,9 +20,8 @@ document.write("
 			<!--The actual menu items. Edit in menu.js-->
 			<script src='javascript/menu.js'></script>
 			<!---->
-			</ul>
-			</nav>
-			<nav style='float:right;'>
+		</nav>
+		<nav style='float:right;'>
 			<ul>
 				<li id='social' style='width:96px;background:none;'>
 					<a href='http://www.facebook.com/FRC3476' class ='icon' >
@@ -42,4 +41,4 @@ document.write("
 ");
 */
 
-document.write("<div id='header'> <div id='spacer' style='height:44px;width:100%;'> </div> <div id='header-content'> <a href='index.html'> <img id='avatar-logo' src='avatar-logo.png'/> </a>  </div> </div> <div id='nav-bar'> <div class='wrapper'> <nav style='float:left;'><ul> <!--The actual menu items. Edit in menu.js--> <script src='javascript/menu.js'></script> <!----> </ul></nav><nav style='float:right;'><ul><li id='social' style='width:96px;background:none;'> <a href='http://www.facebook.com/FRC3476' class ='icon' > <img src='icons/facebook.png' height='100%'/> </a> <a href='http://twitter.com/codeorangefrc' class ='icon' > <img src='icons/twitter.png' height='100%'/> </a> <a href='http://www.youtube.com/user/codeorangefrc' class ='icon'> <img src='icons/youtube.png' height='100%' /> </a> </li> </ul> </nav> </div> </div>");
+document.write("<div id='header'> <div id='spacer' style='height:44px;width:100%;'> </div> <div id='header-content'> <a href='index.html'> <img id='avatar-logo' src='avatar-logo.png'/> </a>  </div> </div> <div id='nav-bar'> <div class='wrapper'> <nav style='float:left;'><!--The actual menu items. Edit in menu.js--> <script src='javascript/menu.js'></script> <!----></nav><nav style='float:right;'><ul><li id='social' style='width:96px;background:none;'> <a href='http://www.facebook.com/FRC3476' class ='icon' > <img src='icons/facebook.png' height='100%'/> </a> <a href='http://twitter.com/codeorangefrc' class ='icon' > <img src='icons/twitter.png' height='100%'/> </a> <a href='http://www.youtube.com/user/codeorangefrc' class ='icon'> <img src='icons/youtube.png' height='100%' /> </a> </li> </ul> </nav> </div> </div>");
--- javascript/menu-drawer-bar.js
+++ javascript/menu-drawer-bar.js
@@ -22,7 +22,7 @@ document.write("
 		</a>
 	</div>
 	<!--The actual menu items. Edit in menu.js-->
-		<script src='javascript/menu.js'></script></ul><div style='height:40px;'></div>
+		<script src='javascript/menu.js'></script><div style='height:40px;'></div>
 	<!---->
 </div>
 <div id='title-bar'>
@@ -33,4 +33,4 @@ document.write("
 ");
 */
 
-document.write("<div id='menu-drawer'> <h3> <a class='nav-li' href='index.html'> <img src='logo-code-orange.png' width='100%'/> </a> </h3> <div id='social' style='float:right;width:120px;background:none;margin-top:-20px;'> <a class='nav-li icon' href='http://www.facebook.com/FRC3476' > <img src='icons/facebook.png' height='100%'/> </a> <a class='nav-li icon' href='http://twitter.com/codeorangefrc' > <img src='icons/twitter.png' height='100%'/> </a> <a class='nav-li icon' href='http://www.youtube.com/user/codeorangefrc' > <img src='icons/youtube.png' height='100%' /> </a></div><ul><li style='padding-top:20px;'><a style='float:left;margin:0;padding-right:0;padding-left:25px;' href='calendar.html' > <img src='icons/calendar-32.png' style='width:auto;height:auto;margin:0;'/> </a><a style='float:left;' href='logs.html' > <img src='icons/docs-32.png' style='width:auto;height:auto;'/> </a><div style='width:100px;height:50px;'></div></li> <!--The actual menu items. Edit in menu.js--> <script src='javascript/menu.js'></script></ul><div style='height:40px;'></div> <!----> </div> <div id='title-bar'> <a href='#' id='menu-button'> </a> <span>Code Orange 3476</span> </div>");
+document.write("<div id='menu-drawer'> <h3> <a class='nav-li' href='index.html'> <img src='logo-code-orange.png' width='100%'/> </a> </h3> <div id='social' style='float:right;width:120px;background:none;margin-top:-20px;'> <a class='nav-li icon' href='http://www.facebook.com/FRC3476' > <img src='icons/facebook.png' height='100%'/> </a> <a class='nav-li icon' href='http://twitter.com/codeorangefrc' > <img src='icons/twitter.png' height='100%'/> </a> <a class='nav-li icon' href='http://www.youtube.com/user/codeorangefrc' > <img src='icons/youtube.png' height='100%' /> </a></div><ul><li style='padding-top:20px;'><a style='float:left;margin:0;padding-right:0;padding-left:25px;' href='calendar.html' > <img src='icons/calendar-32.png' style='width:auto;height:auto;margin:0;'/> </a><a style='float:left;' href='logs.html' > <img src='icons/docs-32.png' style='width:auto;height:auto;'/> </a><div style='width:100px;height:50px;'></div></li> <!--The actual menu items. Edit in menu.js--> <script src='javascript/menu.js'></script><div style='height:40px;'></div> <!----> </div> <div id='title-bar'> <a href='#' id='menu-button'> </a> <span>Code Orange 3476</span> </div>");
